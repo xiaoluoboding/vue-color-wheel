fdaaa17fd0cd5f5263801df54791ffee27fbad2b
Yuwnei Xiao <xiaoluoboding@gmail.com>
2024-02-06 15:40:12 +0800

    chore: change logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,45 +1,75 @@
 <template>
   <svg
+    width="512"
+    height="512"
+    viewBox="0 0 512 512"
+    fill="none"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
-    aria-hidden="true"
-    role="img"
-    class="iconify iconify--logos"
-    width="31.88"
-    height="32"
-    preserveAspectRatio="xMidYMid meet"
-    viewBox="0 0 256 257"
+    class=""
   >
+    <rect
+      id="r4"
+      width="512"
+      height="512"
+      x="0"
+      y="0"
+      rx="128"
+      fill="url(#r5)"
+      stroke="#FFFFFF"
+      stroke-width="0"
+      stroke-opacity="100%"
+      paint-order="stroke"
+    ></rect>
+    <clipPath id="clip">
+      <use xlink:href="#r4"></use>
+    </clipPath>
     <defs>
       <linearGradient
-        id="IconifyId1813088fe1fbc01fb466"
-        x1="-.828%"
-        x2="57.636%"
-        y1="7.652%"
-        y2="78.411%"
+        id="r5"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="rotate(45)"
+        style="transform-origin: center center"
       >
-        <stop offset="0%" stop-color="#41D1FF"></stop>
-        <stop offset="100%" stop-color="#BD34FE"></stop>
+        <stop stop-color="#42D392"></stop>
+        <stop offset="1" stop-color="#155F3E"></stop>
       </linearGradient>
-      <linearGradient
-        id="IconifyId1813088fe1fbc01fb467"
-        x1="43.376%"
-        x2="50.316%"
-        y1="2.242%"
-        y2="89.03%"
+      <radialGradient
+        id="r6"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(256) rotate(90) scale(512)"
       >
-        <stop offset="0%" stop-color="#FFEA83"></stop>
-        <stop offset="8.333%" stop-color="#FFDD35"></stop>
-        <stop offset="100%" stop-color="#FFA800"></stop>
-      </linearGradient>
+        <stop stop-color="white"></stop>
+        <stop offset="1" stop-color="white" stop-opacity="0"></stop>
+      </radialGradient>
     </defs>
-    <path
-      fill="url(#IconifyId1813088fe1fbc01fb466)"
-      d="M255.153 37.938L134.897 252.976c-2.483 4.44-8.862 4.466-11.382.048L.875 37.958c-2.746-4.814 1.371-10.646 6.827-9.67l120.385 21.517a6.537 6.537 0 0 0 2.322-.004l117.867-21.483c5.438-.991 9.574 4.796 6.877 9.62Z"
-    ></path>
-    <path
-      fill="url(#IconifyId1813088fe1fbc01fb467)"
-      d="M185.432.063L96.44 17.501a3.268 3.268 0 0 0-2.634 3.014l-5.474 92.456a3.268 3.268 0 0 0 3.997 3.378l24.777-5.718c2.318-.535 4.413 1.507 3.936 3.838l-7.361 36.047c-.495 2.426 1.782 4.5 4.151 3.78l15.304-4.649c2.372-.72 4.652 1.36 4.15 3.788l-11.698 56.621c-.732 3.542 3.979 5.473 5.943 2.437l1.313-2.028l72.516-144.72c1.215-2.423-.88-5.186-3.54-4.672l-25.505 4.922c-2.396.462-4.435-1.77-3.759-4.114l16.646-57.705c.677-2.35-1.37-4.583-3.769-4.113Z"
-    ></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="352"
+      height="352"
+      viewBox="0 0 24 24"
+      x="80"
+      y="80"
+      alignment-baseline="middle"
+      style="color: rgb(255, 255, 255)"
+    >
+      <g
+        fill="none"
+        stroke="#ffffff"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1.5"
+      >
+        <path
+          d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10"
+        ></path>
+        <path
+          d="M12 16a4 4 0 1 1 0-8a4 4 0 0 1 0 8m0-14v6m0 8v6M2 12h6m8 0h6M4.929 4.929L9.172 9.17m5.656 5.659l4.243 4.242m-14.142 0l4.243-4.242m5.656-5.658l4.243-4.242"
+        ></path>
+      </g>
+    </svg>
   </svg>
 </template>
